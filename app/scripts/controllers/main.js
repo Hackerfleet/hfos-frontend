29e21220248a3a4b5353a655763eddf7f07c8e1f
riot <riot@hackerfleet.org>
2015-02-28 19:06:01 +0100

    First throw of a chat inside

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,13 +8,15 @@
  * Controller of the frontendApp
  */
 angular.module('frontendApp')
-  .controller('MainCtrl', function ($scope, $modal, socket, md5) {
+  .controller('MainCtrl', function ($scope, $modal, $aside, socket, md5) {
     //var ws = ngSocket('ws://localhost:8055/websocket');
 
     //ws.send({Hello: 'bar'});
 
     var loginModal = $modal({scope: $scope, template: 'views/modals/login.tpl.html', show: false});
     var profileModal = $modal({scope: $scope, template: 'views/modals/profile.tpl.html', show: false});
+
+    var chatAside = $aside({scope: $scope, template: 'views/aside/chat.tpl.html', show:false});
 
     $scope.user = { username: '',
                     password: '',
@@ -26,6 +28,42 @@ angular.module('frontendApp')
     $scope.status = { connected: false,
                       signedin: false
     };
+
+    $scope.chat = { messages: [],
+                    input: "",
+                    open: false,
+    };
+
+    $scope.chattoggle = function() {
+        if ($scope.chat.open) {
+            $scope.chatclose();
+        } else {
+            $scope.chatopen();
+        }
+    }
+
+    $scope.chatclose= function() {
+        console.log("Closing down chat.");
+        socket.send({'type': 'chatevent', 'content': 'part'});
+    }
+
+    $scope.chatopen = function() {
+        console.log('Trying to open chat.');
+        if ($scope.status.signedin) {
+            console.log("Opening chat.");
+            chatAside.$promise.then(chatAside.show());
+            socket.send({'type': 'chatevent', 'content': 'join'});
+        }
+
+    //data-template="views/aside/chat.tpl.html" style="border-color: transparent; margin-left:5px;" data-placement="left" data-animation="am-slide-left" bs-aside="aside" data-container="body"
+
+    }
+
+    $scope.chatsend = function() {
+        console.log("Transmitting current message.");
+        socket.send({'type': 'chatmessage', 'content': $scope.chat.input});
+        $scope.chat.input = "";
+    }
 
     $scope.userlogin = function() {
         console.log('Trying to login.');
@@ -81,10 +119,16 @@ angular.module('frontendApp')
     socket.onMessage(function(message) {
         var data = JSON.parse(message.data);
 
-        $scope.status.connected = true;
         console.log(data);
 
-        if(data.type === 'warning') {
+        if(data.type === 'info') {
+          if (data.content === 'Connected') {
+            console.log('Client connected to HFOS');
+            $scope.status.connected = true;
+          } else {
+            console.log('Unknown info message:', data.content);
+          }
+        } else if(data.type === 'warning') {
           var warningmodal = $modal({title: 'Warning!', content: String(data.content), show:true});
         } else if(data.type === 'auth') {
             console.log('Got an auth packet!');
@@ -98,7 +142,10 @@ angular.module('frontendApp')
             } else {
                 console.log('Failed to log in.');
             }
-
-        }
+        } else if(data.type === 'chat') {
+            console.log('Incoming chat data: ', data);
+            $scope.chat.messages.push(data.content);
+            console.log($scope.chat);
+        };
     });
   });
