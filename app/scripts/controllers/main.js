c04e874c0733f52004ca496eb82531da7edb302d
riot <riot@hackerfleet.org>
2015-07-25 18:30:59 +0200

    Reformatting and schemata check

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,59 +8,62 @@
  * Controller of the hfosFrontendApp
  */
 angular.module('hfosFrontendApp')
-  .controller('MainCtrl', function ($scope, $route, $modal, $aside, $interval, schemata, socket, user) {
+    .controller('MainCtrl', function ($scope, $route, $modal, $aside, $interval, schemata, socket, user) {
 
-    var chatAside = $aside({scope: $scope, template: 'views/aside/chat.tpl.html', show:false, backdrop: false});
+        var chatAside = $aside({scope: $scope, template: 'views/aside/chat.tpl.html', show: false, backdrop: false});
 
         $scope.isChatOpen = false;
 
         socket.send({'type': 'info', 'content': 'Main Controller activated'});
+
         $('#bootscreen').hide();
-    $scope.home = function(event) {
-        if (event.shiftKey === true) {
-            console.log('Reloading route.');
-            $route.reload();
-        } else if (event.ctrlKey === true) {
-            console.log('Disconnecting');
-            socket.disconnect();
-        }
-        socket.check();
-        user.check();
-        console.log('Main profile: ', user.profile());
-    };
 
-    $scope.userbutton = function(event) {
-        console.log('USERBUTTON: ', event);
-        user.login();
-        //user.onAuth(loginModal.hide);
-    };
+        $scope.home = function (event) {
+            if (event.shiftKey === true) {
+                console.log('Reloading route.');
+                $route.reload();
+            } else if (event.ctrlKey === true) {
+                console.log('Disconnecting');
+                socket.disconnect();
+            }
+            socket.check();
+            user.check();
+            schemata.check();
+            console.log('Main profile: ', user.profile());
+        };
 
-    $scope.chattoggle = function() {
-        if ($scope.isChatOpen) {
-            $scope.chatclose();
-        } else {
-            $scope.chatopen();
-        }
-    };
+        $scope.userbutton = function (event) {
+            console.log('USERBUTTON: ', event);
+            user.login();
+            //user.onAuth(loginModal.hide);
+        };
 
-    $scope.chatclose= function() {
-        console.log('Closing down chat.');
-        chatAside.hide();
-        $('#btnchat').css('color', '');
-        $scope.isChatOpen = false;
-    };
+        $scope.chattoggle = function () {
+            if ($scope.isChatOpen) {
+                $scope.chatclose();
+            } else {
+                $scope.chatopen();
+            }
+        };
 
-    $scope.chatopen = function() {
-        console.log('Trying to open chat.');
-        if (user.signedin()) {
-            console.log('Opening chat.');
-            chatAside.$promise.then(chatAside.show());
+        $scope.chatclose = function () {
+            console.log('Closing down chat.');
+            chatAside.hide();
+            $('#btnchat').css('color', '');
+            $scope.isChatOpen = false;
+        };
 
-            $('#btnchat').css('color', '#0f0');
-            $scope.isChatOpen = true;
-            $('#chatinput').focus();
-        }
-    };
+        $scope.chatopen = function () {
+            console.log('Trying to open chat.');
+            if (user.signedin()) {
+                console.log('Opening chat.');
+                chatAside.$promise.then(chatAside.show());
+
+                $('#btnchat').css('color', '#0f0');
+                $scope.isChatOpen = true;
+                $('#chatinput').focus();
+            }
+        };
 
 
     });
