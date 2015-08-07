a962a8ff4e31a13c861c38fd677e56b46ac6fc10
riot <riot@hackerfleet.org>
2015-08-08 00:58:29 +0200

    MOB button fix and alert service

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,9 +8,10 @@
  * Controller of the hfosFrontendApp
  */
 angular.module('hfosFrontendApp')
-    .controller('MainCtrl', function ($scope, $route, $modal, $aside, $interval, schemata, socket, user) {
+    .controller('MainCtrl', function ($scope, $route, $modal, $aside, $interval, Alert, schemata, socket, user) {
 
-        console.log('[MAIN] ########', $aside);
+        console.log('[MAIN] Controller started');
+
         var chatAside = $aside({
             scope: $scope,
             template: 'views/aside/chat.tpl.html',
@@ -19,10 +20,6 @@ angular.module('hfosFrontendApp')
         });
 
         $scope.isChatOpen = false;
-        $scope.alerting = false;
-
-        var blinkstate = true;
-        var blinker = '';
 
         socket.send({'type': 'info', 'content': 'Main Controller activated'});
 
@@ -30,52 +27,29 @@ angular.module('hfosFrontendApp')
 
         $scope.home = function (event) {
             if (event.shiftKey === true) {
-                console.log('Reloading route.');
+                console.log('[MAIN] Reloading route.');
                 $route.reload();
             } else if (event.ctrlKey === true) {
-                console.log('Disconnecting');
+                console.log('[MAIN] Disconnecting');
                 socket.disconnect();
             }
             socket.check();
             user.check();
             schemata.check();
-            console.log('Main profile: ', user.profile());
+            console.log('[MAIN] Main profile: ', user.profile());
         };
 
         $scope.userbutton = function (event) {
-            console.log('USERBUTTON: ', event);
+            console.log('[MAIN] USERBUTTON: ', event);
             user.login();
             //user.onAuth(loginModal.hide);
         };
 
-        var mobBlinker = function () {
-            console.log('[MAIN] MOB Button Blinkstate:', blinkstate);
-            if (blinkstate === false) {
-                blinkstate = true;
-                $('#btnmob').css('background', '#FF2222');
-            } else {
-                blinkstate = false;
-                $('#btnmob').css('background', '#772222');
-            }
+        $scope.mobbutton = function () {
+            console.log('[MAIN] MOB Button pressed');
+            Alert.mobTrigger();
         };
 
-        $scope.mobAlert = function () {
-            if ($scope.alerting === false) {
-                console.log('[MAIN] ManOverBoard alert triggered!');
-                $('#btnmob').css('background', '#AA2222').css('color', '#FF0');
-                $scope.alerting = true;
-                blinker = $interval(mobBlinker, 1500);
-                socket.send({'component': 'alert', 'action': 'mob', 'data': true});
-
-            } else {
-                console.log('[MAIN] ManOverBoard alert deactivated.');
-                // TODO: We shouldn'T be able to toggle this off - only the node may!
-                $scope.alerting = false;
-                socket.send({'component': 'alert', 'action': 'mob', 'data': true});
-                $interval.cancel(blinker);
-                $('#btnmob').css('background', '').css('color', '#F00');
-            }
-        };
 
         $scope.chattoggle = function () {
             if ($scope.isChatOpen) {
@@ -86,16 +60,16 @@ angular.module('hfosFrontendApp')
         };
 
         $scope.chatclose = function () {
-            console.log('Closing down chat.');
+            console.log('[MAIN] Closing down chat.');
             chatAside.hide();
             $('#btnchat').css('color', '');
             $scope.isChatOpen = false;
         };
 
         $scope.chatopen = function () {
-            console.log('Trying to open chat.');
+            console.log('[MAIN] Trying to open chat.');
             if (user.signedin()) {
-                console.log('Opening chat.');
+                console.log('[MAIN] Opening chat.');
                 chatAside.$promise.then(chatAside.show());
 
                 $('#btnchat').css('color', '#0f0');
---
+++ app/scripts/services/alert.js
@@ -0,0 +1,113 @@
+'use strict';
+
+/**
+ * @ngdoc service
+ * @name hfosFrontendApp.Alert
+ * @description
+ * # Alert
+ * Service in the hfosFrontendApp.
+ */
+angular.module('hfosFrontendApp')
+    .service('Alert', function ($rootScope, $interval, socket, createDialog) {
+
+        console.log('[ALERT] Service ready.');
+
+        var blinkstate = true;
+        var blinker = '';
+        var alerting = false;
+
+        var mobBlinker = function () {
+            if (blinkstate === false) {
+                blinkstate = true;
+                $('#mainmenu').css('background', '#FF2222');
+            } else {
+                blinkstate = false;
+                $('#mainmenu').css('background', '#772222');
+            }
+        };
+
+        var mobAlert = function () {
+            if (alerting === false) {
+                console.log('[ALERT] ManOverBoard alert triggered!');
+                $('#mainmenu').css('background', '#AA2222');
+                $('#btnmob').css('color', '#FF0');
+                alerting = true;
+                blinker = $interval(mobBlinker, 1500);
+            }
+        };
+
+        var mobDeactivate = function () {
+            if (alerting === true) {
+                console.log('[ALERT] ManOverBoard alert deactivated.');
+                alerting = false;
+                $interval.cancel(blinker);
+                $('#mainmenu').css('background', '');
+                $('#btnmob').css('color', '#F00');
+            }
+        };
+
+        var mobTrigger = function () {
+            if (alerting === true) {
+                showDeactivate();
+            } else {
+                console.log('[ALERT] MOB Alarm triggered. Transmitting to node.');
+                socket.send({'component': 'alert', 'action': 'mob', 'data': true});
+            }
+        };
+
+        var deactivateAlarm = function () {
+            console.log('[ALERT] Deactivating client local alert.');
+            mobDeactivate();
+        };
+
+        var cancelAlarm = function () {
+            console.log('[ALERT] Requesting cancellation of alert upon user request.');
+            socket.send({'component': 'alert', 'action': 'mob', 'data': false});
+        };
+
+
+        var showDeactivate = function () {
+            createDialog('/views/modals/alertdeactivation.tpl.html', {
+                    id: 'alertdeactivationDialog',
+                    title: 'Deactivate Alert',
+                    backdrop: false,
+                    footerTemplate: '<span></span>'
+                }
+            );
+        };
+
+        socket.onMessage(function (message) {
+            // Alert Handler
+            var msg = JSON.parse(message.data);
+
+            if (msg.component === 'alert') {
+                console.log('[ALERT] Got an alert packet!');
+
+                if (msg.action === 'mob') {
+                    console.log('[ALERT] Activating MOB alert!');
+                    if (msg.data === true) {
+                        mobAlert();
+                    } else if (msg.data === false) {
+                        mobDeactivate();
+                    }
+                }
+            }
+        });
+
+        $rootScope.$on('User.Login', function () {
+            $('#btnmob').removeClass('hidden');
+        });
+
+        return {
+            mobTrigger: mobTrigger,
+            cancelAlarm: cancelAlarm,
+            deactivateAlarm: deactivateAlarm
+        };
+    }).controller('AlertCtrl', function ($scope, Alert) {
+        console.log('[ALERT] AlertCtrl loaded!');
+
+        $scope.cancelAlarm = Alert.cancelAlarm;
+        $scope.deactivateAlarm = Alert.deactivateAlarm;
+
+        $('#cancel').focus();
+    });
