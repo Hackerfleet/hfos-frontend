24a0a28fa9499f9b266f973987501226d27deb7d
riot <riot@hackerfleet.org>
2015-08-10 01:41:43 +0200

    Socket now handling disconnects more graceful

--- app/scripts/services/socket.js
+++ app/scripts/services/socket.js
@@ -8,11 +8,19 @@
  * Factory in the hfosFrontendApp.
  */
 angular.module('hfosFrontendApp')
-    .factory('socket', ['ngSocket', '$location', function (ngSocket, $location) {
+    .factory('socket', function (ngSocket, $timeout, $location) {
 
         var host = $location.host();
         var port = 8055;
+
         var sock = ngSocket('ws://' + host + ':' + port + '/websocket');
+        var reconnecttimer = '';
+        var reconnecttries = 0;
+
+        var stayonline = true;
+        var connected = false;
+        var trying = false;
+
         sock.onMessage(function (args) {
             console.log(args);
         });
@@ -21,20 +29,56 @@ angular.module('hfosFrontendApp')
             return host;
         };
 
-        var connected = false;
+        var doReconnect = function () {
+            console.log('[SOCKET] Trying to reconnect.');
+            sock.reconnect();
 
-        var OpenEvent = function (args) {
-            console.log('[SOCKET] Websocket successfully opened!', args);
+            reconnecttries++;
+            trying = true;
+
+            var interval = Math.min(30, Math.pow(reconnecttries, 2)) * 1000;
+            reconnecttimer = $timeout(doReconnect, interval);
+            console.log('[SOCKET] Resetting interval to ', interval);
+            //reconnecttimer = $interval(doReconnect, interval);
+        };
+
+        var finallyClosed = function () {
+            console.log('[SOCKET] Something closed the websocket!');
+            connected = false;
+            $('#btnhome').css('color', '#f00');
+            $('#btnmob').addClass('hidden');
+            // TODO: Mob button should rather be gray and recording the MOB alert for later,
+            // if possible with local GPS coords
+            $('#btnuser').css('color', '#fa0').addClass('hidden');
+        };
+
+        var OpenEvent = function () {
+            console.log('[SOCKET] Websocket successfully opened!');
             connected = true;
+            stayonline = true;
+            trying = false;
+            reconnecttries = 0;
+
+            $timeout.cancel(reconnecttimer);
             $('#btnhome').css('color', '#3a75a8');
             $('#btnuser').removeClass('hidden').css('color', '');
         };
 
-        var CloseEvent = function (args) {
-            console.log('[SOCKET] Something closed the websocket!', args);
+        var CloseEvent = function () {
+            console.log('[SOCKET] Close event called.');
+
             connected = false;
-            $('#btnhome').css('color', '#f00');
-            $('#btnuser').css('color', '#fa0').addClass('hidden');
+            $('#btnhome').css('color', '#FF0');
+
+            if (trying === true) {
+                console.log('[SOCKET] Already trying');
+                return;
+            }
+            if (stayonline === true) {
+                doReconnect();
+            } else {
+                finallyClosed();
+            }
         };
 
         sock.onOpen(OpenEvent);
@@ -45,6 +89,7 @@ angular.module('hfosFrontendApp')
         };
 
         var doDisconnect = function () {
+            stayonline = false;
             sock.close();
         };
 
@@ -85,4 +130,4 @@ angular.module('hfosFrontendApp')
                 return sock.onClose(func);
             }
         };
-    }]);
+    });
