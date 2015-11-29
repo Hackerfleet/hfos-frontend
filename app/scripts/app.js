151cd8420d137fe2a6bfcb79f5e94bd5dbd2db53
riot <riot@hackerfleet.org>
2015-11-29 18:01:25 +0100

    Cleanups and new controllers added

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -19,21 +19,25 @@ angular
         'ngSanitize',
         'ngTouch',
         'angular-md5',
+        'pascalprecht.translate',
         'leaflet-directive',
         'fundoo.services',
         'schemaForm',
         'schemaForm-tinymce',
         'angular-detector',
         'routeStyles',
-        // 'angularDeckster',
         'ngFitTextDynamic',
         'ngRadialGauge',
-        'gridster'
+        'gridster',
+        'frapontillo.bootstrap-switch',
+        'ui.select',
+        'mgcrea.ngStrap.select',
+        'ngDraggable'
     ])
     .config(function ($routeProvider) {
         $routeProvider
             .when('/', {
-                templateUrl: 'views/main.html',
+                templateUrl: 'views/main.html'
                 //controller: 'MainCtrl'
             })
             .when('/about', {
@@ -58,17 +62,28 @@ angular
                 controller: 'GamepadRemoteCtrl',
                 css: 'styles/gamepadremote.css'
             })
+            .when('/library', {
+                templateUrl: 'views/library.html',
+            })
+            .when('/tasks', {
+                templateUrl: 'views/taskgrid.html',
+                controller: 'TaskGridCtrl'
+            })
             .when('/wiki', {
                 templateUrl: 'views/wiki.html',
-                controller: 'WikiCtrl',
+                controller: 'WikiCtrl'
             })
             .when('/wiki/:slug*', {
                 templateUrl: 'views/wiki.html',
                 controller: 'WikiCtrl'
             })
-            .when('/obj/:schema/:uuid', {
+            .when('/obj/:schema/:uuid/:action?', {
                 templateUrl: 'views/objecteditor.html',
                 controller: 'ObjectEditorCtrl'
+            })
+            .when('/list/:schema', {
+                templateUrl: 'views/objectlist.html',
+                controller: 'ObjectListCtrl'
             })
             .when('/docs', {
                 templateUrl: 'views/docs.html'
@@ -77,48 +92,5 @@ angular
             .otherwise({
                 redirectTo: '/'
             });
-        /*
-        decksterConfigProvider.set({
-         decks: {
-                'Dashboard': {
-                    'cards': [
-                        {
-                            id: 'bearing',
-                            size: {x: 2, y: 1},
-                            position: [2, 0],
-                            value: 'Heading_True'
-                        },
-                        {
-                            id: 'gauge',
-                            size: {x: 2, y: 2},
-                            position: [0, 2],
-                            value: 'GPS_SatCount'
-                        },
-                        {
 
-                            id: 'digital',
-                            size: {x: 2, y: 1},
-                            position: [0, 0],
-                            value: 'GPS_LatLon'
-                        }]
-                }
-            },
-            cardDefaults: {
-                'bearing': {
-                    title: 'Bearing',
-                    summaryTemplateUrl: 'views/cards/courseSummaryTemplate.html',
-                    detailTemplateUrl: 'views/cards/courseDetailsTemplate.html'
-                },
-                'gauge': {
-                    title: 'Gauge',
-                    summaryTemplateUrl: 'views/cards/analogReadoutSummaryTemplate.html',
-                    detailTemplateUrl: 'views/cards/analogReadoutDetailsTemplate.html'
-                },
-                'digital': {
-                    title: 'Digital',
-                    summaryTemplateUrl: 'views/cards/digitalReadoutSummaryTemplate.html',
-                    detailTemplateUrl: 'views/cards/digitalReadoutDetailsTemplate.html'
-                }
-            }
-         }); */
     });
