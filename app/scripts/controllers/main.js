09dfda3bd3857859897f02fa87674eb8fa78b587
riot <riot@hackerfleet.org>
2015-11-29 18:04:23 +0100

    Menu items (preliminary, has to be constructed, actually)

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -22,6 +22,95 @@ angular.module('hfosFrontendApp')
         $scope.isChatOpen = false;
         $scope.signedin = false;
 
+        // TODO: Move this to the client configuration as server supplied module list or something
+        $scope.items = [
+            {
+                title: 'Map',
+                url: 'map',
+                svg: 'iconmonstr-map-2-icon.svg',
+                row: 0,
+                col: 0
+            },
+            {
+                title: 'Switchboard',
+                url: 'switchboard',
+                svg: 'iconmonstr-control-panel-icon.svg',
+                row: 0,
+                col: 1
+            },
+            {
+                title: 'Communication',
+                url: 'communication',
+                svg: 'iconmonstr-radio-tower-icon.svg',
+                row: 0,
+                col: 2
+            },
+            {
+                title: 'Wiki',
+                url: 'wiki/Index',
+                svg: 'iconmonstr-note-21-icon.svg',
+                row: 0,
+                col: 3
+            },
+            {
+                title: 'Weather',
+                url: 'weather',
+                svg: 'iconmonstrandhackerfleet-weather-icon.svg',
+                row: 1,
+                col: 0
+            },
+            {
+                title: 'Logbook',
+                url: 'logbook',
+                svg: 'iconmonstr-printer-icon.svg',
+                row: 1,
+                col: 1
+            },
+            {
+                title: 'Dashboard',
+                url: 'dashboard',
+                svg: 'iconmonstr-compass-6-icon.svg',
+                row: 1,
+                col: 2
+            },
+            {
+                title: 'Settings',
+                url: 'settings',
+                svg: 'iconmonstr-wrench-4-icon.svg',
+                row: 1,
+                col: 3
+            }, {
+                title: 'Remote Control',
+                url: 'gamepadremote',
+                svg: 'iconmonstr-gamepad-2-icon.svg',
+                row: 2,
+                col: 0
+            }, {
+                title: 'Library',
+                url: 'library',
+                svg: 'iconmonstr-mediamashup-icon.svg',
+                row: 2,
+                col: 1
+            }, {
+                title: 'Tasks',
+                url: 'tasks',
+                svg: 'iconmonstr-clipboard-4-icon.svg',
+                row: 2,
+                col: 2
+
+            }
+        ];
+
+
+        $scope.gridsterOptions = {
+            // any options that you can set for angular-gridster (see:  http://manifestwebdesign.github.io/angular-gridster/)
+            columns: screen.width / 150,
+            rowHeight: 150,
+            colWidth: 150,
+
+            margins: [5, 5]
+        };
+
         socket.send({'type': 'info', 'content': 'Main Controller activated'});
 
         $('#bootscreen').hide();
