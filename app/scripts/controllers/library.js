84cd60affca7c6d75d9f6d61ee1d4711ed28f0a8
riot <riot@hackerfleet.org>
2015-11-29 18:11:55 +0100

    Library added to frontend

---
+++ app/scripts/controllers/library.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name hfosFrontendApp.controller:LibraryCtrl
+ * @description
+ * # LibraryCtrl
+ * Controller of the hfosFrontendApp
+ */
+angular.module('hfosFrontendApp')
+    .controller('LibraryCtrl', function ($scope) {
+
+    });
