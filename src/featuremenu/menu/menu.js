718643cde3b0e1c28df9ef7bad4bd916b53f7334
riot <riot@hackerfleet.org>
2016-09-08 14:38:34 +0200

    Less verbosity

--- src/featuremenu/menu/menu.js
+++ src/featuremenu/menu/menu.js
@@ -147,7 +147,7 @@ class featureMenu {
                         item.col = configentry.col;
                         item.size = configentry.size;
                     } catch (e) {
-                        console.log("Oh, no menu config: ", e, state.label);
+                        console.log("Oh, no menu config: ", state.label);
                         item.row = row;
                         item.col = col;
                         item.size = 1;
