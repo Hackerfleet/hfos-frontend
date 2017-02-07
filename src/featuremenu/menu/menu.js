078dc126742c76ed4464d11327d4ebdd2227b799
riot <riot@c-base.org>
2017-02-07 19:50:47 +0100

    Only store on changed cards

--- src/featuremenu/menu/menu.js
+++ src/featuremenu/menu/menu.js
@@ -1,5 +1,5 @@
 class featureMenu {
-
+    
     constructor(userservice, $state, $log, $scope, $timeout, alert) {
         this.signedin = false;
         this.state = $state;
@@ -7,16 +7,16 @@ class featureMenu {
         this.scope = $scope;
         this.alert = alert;
         this.changetimeout = null;
-    
+        
         $log.log('[MENU] Featuremenu initializing with Profile: ', userservice.profile);
         console.log(userservice.profile);
-    
+        
         var self = this;
         
         this.storeMenuConfig = function () {
             if ('settings' in self.user.profile) {
                 console.log('[MENU] Updating menu list for profile:', self.items);
-    
+                
                 var menu = [];
                 
                 for (var item of self.items) {
@@ -35,13 +35,15 @@ class featureMenu {
             self.changetimeout = null;
         };
         
-        $scope.$watch('$ctrl.items', function(items){
-            if (self.changetimeout !== null) {
-                $timeout.cancel(self.changetimeout);
+        $scope.$watch('$ctrl.items', function (newVal, oldVal) {
+            if (newVal !== oldVal) {
+                if (self.changetimeout !== null) {
+                    $timeout.cancel(self.changetimeout);
+                }
+                self.changetimeout = $timeout(self.storeMenuConfig, 2000);
             }
-            self.changetimeout = $timeout(self.storeMenuConfig, 2000);
         }, true);
-
+        
         this.gridsterOptions = {
             // any options that you can set for angular-gridster (see:  http://manifestwebdesign.github.io/angular-gridster/)
             columns: screen.width / 100,
@@ -49,11 +51,11 @@ class featureMenu {
             colWidth: 100,
             margins: [5, 5]
         };
-
-
+        
+        
         $('#bootscreen').hide();
-
-
+        
+        
         // TODO: Kept for reference until all icons are moved to their respecting packages:
         /*this.items = [
          {
@@ -116,42 +118,41 @@ class featureMenu {
          svg: 'iconmonstr-clipboard-4-icon.svg',
          row: 2,
          col: 2
-
+         
          }
          ];*/
-
-
-
-        this.updateMenu = function() {
+        
+        
+        this.updateMenu = function () {
             console.log('[MENU] Updating featuremenus');
             var row = 0, col = 0;
-
+            
             self.items = [];
-
+            
             var menu = $('#modulemenu');
-
+            
             menu.empty();
-
-
+            
+            
             // TODO: Move these two into the user service. Actually, stuff should be available per default.
             if (typeof self.user.profile.settings === 'undefined') {
                 self.user.profile.settings = {};
             }
-
+            
             if (typeof self.user.profile.settings.menu === 'undefined') {
                 self.user.profile.settings.menu = [];
             }
             
             console.log('[MENU] Settings:', self.user.profile.settings.menu);
-
+            
             for (var state of self.state.get()) {
                 var enabled = [];
                 var profile = self.user.profile;
-
+                
                 if (typeof profile.components !== 'undefined') {
                     enabled = profile.components.enabled;
                 }
-
+                
                 if ('icon' in state && true) { // (state.label === 'Map' || state.label in enabled)) {
                     var item = {
                         title: state.label,
@@ -161,7 +162,7 @@ class featureMenu {
                         col: 0,
                         size: 1
                     };
-
+                    
                     try {
                         console.log("[MENU] Attempt:", profile.settings.menu, state.label);
                         console.log(profile.settings.menu[0].title, state.label);
@@ -175,7 +176,7 @@ class featureMenu {
                         item.row = row;
                         item.col = col;
                         item.size = 1;
-
+                        
                         if (typeof profile.components !== 'undefined') {
                             var entry = {
                                 title: item.title,
@@ -185,26 +186,26 @@ class featureMenu {
                             };
                             console.log('[MENU] Pushing to menu config: ', entry);
                             profile.settings.menu.push(entry);
-
+                            
                             //self.user.storeprofile();
                         }
-
+                        
                         col++;
                         if (col === 5) {
                             col = 0;
                             row++;
                         }
                     }
-
+                    
                     var menuentry = '<li><a href="#' + item.url + '"><img class="module-icon-tiny" src="' + item.svg + '" type="image/svg+xml">' + item.title + '</a></li>';
                     menu.append(menuentry);
-
+                    
                     self.items.push(item);
                 } else {
                     console.log('[MENU] Item has either no icon or is disabled:', state);
                 }
             }
-
+            
             if (self.items.length === 0) {
                 // TODO: Add link to docs, that explains how to do that.
                 this.alert.add('warning', 'No modules', 'The menu is empty. A probable reason could be that you have no modules installed.');
@@ -212,7 +213,7 @@ class featureMenu {
             
             console.log('[MENU] Menudata:', self.items, self.user.profile.settings.menu);
         };
-
+        
         if (typeof this.user.profile.settings !== 'undefined') {
             if (typeof this.user.profile.settings.menu !== 'undefined') {
                 this.updateMenu();
