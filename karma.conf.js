0e2b2633ec947bcd95d86ed9ef3dc1b48bb22384
riot <riot@c-base.org>
2017-05-18 17:08:14 +0200

    On the way to fixed frontend testing

--- karma.conf.js
+++ karma.conf.js
@@ -1,4 +1,4 @@
-var webpackConfig = require('./webpack.test');
+var webpack = require('webpack');
 
 // Reference: http://karma-runner.github.io/0.12/config/configuration-file.html
 module.exports = function karmaConfig(config) {
@@ -8,42 +8,60 @@ module.exports = function karmaConfig(config) {
             // Set framework to jasmine
             'jasmine'
         ],
-
+        
         reporters: [
             // Reference: https://github.com/mlex/karma-spec-reporter
             // Set reporter to print detailed results to console
             'spec',
-
+            
             // Reference: https://github.com/karma-runner/karma-coverage
             // Output code coverage files
             'coverage'
         ],
-
+        
         files: [
             // Grab all files in the app folder that contain .test.
             'src/tests.webpack.js'
         ],
-
+        
         preprocessors: {
             // Reference: http://webpack.github.io/docs/testing.html
             // Reference: https://github.com/webpack/karma-webpack
             // Convert files with webpack and load sourcemaps
             'src/tests.webpack.js': ['webpack', 'sourcemap']
         },
-
+        
         browsers: [
-            // Run tests using PhantomJS
-            'PhantomJS'
+            // Run tests using Chrome/Chromium
+            'Chrome'
         ],
-
+        
         singleRun: true,
-
+        
         // Configure code coverage reporter
         coverageReporter: {
             dir: 'build/coverage/',
             type: 'html'
         },
-
-        webpack: webpackConfig
+        
+        webpack: {
+            devtool: 'eval',
+            module: {
+                loaders: [
+                    {test: /\.js$/, loader: 'babel-loader', exclude: /(\.test.js$|node_modules)/},
+                    {test: /\.css$/, loader: 'style!css-loader'},
+                    {test: /\.tpl.html/, loader: 'html-loader'},
+                    {test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2)$/, loader: 'url-loader?limit=50000'}
+                ]
+            },
+            plugins: [
+                new webpack.LoaderOptionsPlugin({
+                   debug: true
+                }),
+                new webpack.ProvidePlugin({
+                    'window.jQuery': 'jquery'
+                })
+            ]
+        }
     });
 };
