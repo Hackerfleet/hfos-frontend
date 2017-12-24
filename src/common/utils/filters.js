5c4b7a0134be9752b869dd483fb1fc60df3e28e9
riot <riot@c-base.org>
2017-12-24 16:06:19 +0100

    Added to-length filter

--- src/common/utils/filters.js
+++ src/common/utils/filters.js
@@ -21,12 +21,16 @@
  * Created by riot on 08.07.17.
  */
 
-export default function () {
-    return function (input, all) {
-        let reg = (all) ? /([^\W_]+[^\s-]*) */g : /([^\W_]+[^\s-]*)/;
-        return (!!input) ? input.replace(reg, function (txt) {
-                return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
-            }) : '';
-    }
+function capitalize(input, all) {
+    let reg = (all) ? /([^\W_]+[^\s-]*) */g : /([^\W_]+[^\s-]*)/;
+    return (input) ? input.replace(reg, function (txt) {
+        return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+    }) : '';
 }
+
+function toLength(input) {
+    return input.slice(0, 50);
+}
+
+export {capitalize, toLength};
 
