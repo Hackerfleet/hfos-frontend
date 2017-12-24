96914d672f51429d8f0adc6d4d5b064e87c39f3f
riot <riot@c-base.org>
2017-12-24 16:04:45 +0100

    Simplified stats, adapted to new alert

--- src/about/about-component/about-component.js
+++ src/about/about-component/about-component.js
@@ -21,30 +21,28 @@ let showAngularStats = require('ng-stats');
 //showAngularStats();
 
 class AboutComponent {
-    
-    constructor(rootscope, user, socket, $interval, alert, modal, schemaservice, op, state, localStorageService) {
+
+    constructor(rootscope, user, socket, $interval, notification, modal, schemaservice, op, state, localStorageService) {
         this.rootscope = rootscope;
         this.user = user;
         this.socket = socket;
-        this.alert = alert;
+        this.notification = notification;
         this.modal = modal;
         this.$interval = $interval;
         this.state = state;
         this.schemaservice = schemaservice;
         this.storage = localStorageService;
-        
+
         this.schemata = [];
         this.debug = false;
-        this.updater = false;
-        this.stats = {};
-        
+
         this.serverport = socket.port;
         this.ssloverride = socket.protocol === 'wss';
-        
+
         this.consoleinput = '';
-        
+
         $('#path').css({fill: '#afafff'});
-        
+
         this.testOptions = {
             max: 10,
             min: 1,
@@ -53,12 +51,12 @@ class AboutComponent {
         };
         this.sliderValue = 5;
         this.colorValue = "";
-        
+
         let self = this;
-        
+
         function updateSchemata() {
             self.schemata = [];
-            
+
             for (let schema in self.schemaservice.schemata) {
                 if (self.schemaservice.schemata.hasOwnProperty(schema)) {
                     self.schemata.push(schema);
@@ -66,29 +64,18 @@ class AboutComponent {
             }
             console.log(self.schemata);
         }
-        
+
         this.rootscope.$on('Schemata.Update', updateSchemata);
-        
+
         if (this.user.debug) {
             console.log('[ABOUT] Opening debug tab:', this.user.debug);
             this.toggleDebug();
             this.opentab(this.storage.get('debugTab'));
         }
-        
+
         updateSchemata();
     }
-    
-    updateStats() {
-        //console.log("Update: ", this.socket);
-        let stats = this.socket.stats;
-        this.stats = {
-            rx: stats.rx,
-            tx: stats.tx,
-            start: stats.start,
-            lag: 'N/A'
-        };
-    }
-    
+
     command(cmd) {
         this.socket.send({
             'component': 'hfos.events.system',
@@ -96,31 +83,27 @@ class AboutComponent {
             'data': cmd
         });
         let msg = 'Sent: ' + cmd;
-        
-        this.alert.add('info', 'Debugger', msg, 5);
+
+        this.notification.add('info', 'Debugger', msg, 5);
     }
-    
+
     toggleDebug() {
         console.log('[ABOUT] Toggling Debug tools');
         if (this.debug !== true) {
             this.debug = true;
-            
+
             showAngularStats({
                 position: 'bottom',
                 htmlId: 'ngstats'
-                
+
             });
-            this.updateStats();
-            this.updater = this.$interval(() => this.updateStats(), 1000);
         } else {
             this.debug = false;
-            this.$interval.cancel(this.updater);
-            this.updater = false;
         }
-        
+
         this.user.debug = this.debug;
     }
-    
+
     opentab(tabname) {
         console.log('[ABOUT] Switching tab to ', tabname);
         // TODO: De-jquery this (also somewhere else, e.g. dashboard)
@@ -128,42 +111,42 @@ class AboutComponent {
         $('#' + tabname).addClass('active');
         this.storage.set('debugTab', tabname);
     }
-    
+
     viewlist(schema) {
         this.state.go('app.list', {schema: schema});
     }
-    
+
     setserverport() {
         console.log('[ABOUT] Updating server port to ', this.serverport, this.ssloverride);
         this.socket.setPort(this.serverport, this.ssloverride);
     }
-    
+
     changeport(ev) {
         console.log('CHANGE HANDLER CALLED', ev);
         if (this.serverport === 443) {
             this.ssloverride = true;
         }
     }
-    
+
     unsetserverport() {
         console.log('[ABOUT] Removing server port override');
         this.socket.unsetPort();
     }
-    
+
     sendcommand() {
         this.command(this.consoleinput);
     }
-    
+
     memdebug() {
         this.command('memdebug');
     }
-    
+
     graph() {
         this.command('graph');
     }
-    
+
 }
 
-AboutComponent.$inject = ['$rootScope', 'user', 'socket', '$interval', 'alert', '$modal', 'schemata', 'objectproxy', '$state', 'localStorageService'];
+AboutComponent.$inject = ['$rootScope', 'user', 'socket', '$interval', 'notification', '$modal', 'schemata', 'objectproxy', '$state', 'localStorageService'];
 
 export default AboutComponent;
