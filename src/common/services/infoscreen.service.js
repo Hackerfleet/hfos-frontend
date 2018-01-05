82d70021e4cb85cd8a5906a92c04b0ea87d982bc
riot <riot@c-base.org>
2018-01-05 21:31:11 +0100

    License changed to AGPLv3, Copyright updated for 2018 Happy New Year! <3 :fireworks: :wine: :cocktail: :beers:

--- src/common/services/infoscreen.service.js
+++ src/common/services/infoscreen.service.js
@@ -1,19 +1,19 @@
 /*
  * Hackerfleet Operating System
  * ============================
- * Copyright (C) 2011 - 2017 riot <riot@c-base.org> and others.
+ * Copyright (C) 2011 - 2018 riot <riot@c-base.org> and others.
  *
  * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
+ * it under the terms of the GNU Affero General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ * GNU Affero General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License
+ * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
@@ -29,51 +29,51 @@ class InfoscreenService {
         this.rootscope = $rootScope;
         this.state = $state;
         this.timeout = $timeout;
-        
+
         this.rotations = [];
         this.current_screen = 0;
         this.timer = null;
         this.enabled = false;
-                
+
         let self = this;
-    
+
         this.rotate = function() {
             console.log('Rotating screen');
             self.current_screen++;
             if (self.current_screen === self.rotations.length) {
                 self.current_screen = 0;
             }
-        
+
             let nextstate = self.rotations[self.current_screen];
             console.log('WOULD NOW ROTATE TO:', nextstate);
-            
+
             let kwargs = {};
             for (let arg of nextstate.args) {
                 kwargs[arg.name] = arg.value;
             }
-            
+
             console.log(nextstate.state, kwargs);
             $state.go('app.' + nextstate.state, kwargs);
             self.timeout.cancel(self.timer);
             self.timer = self.timeout(self.rotate, self.rotations[self.current_screen].duration*1000);
         }
     }
-    
+
     setRotations(rotations) {
         console.log('Setting rotations to ', rotations);
         this.rotations = rotations
     }
-    
-    
+
+
     toggleRotations(state) {
         console.log('New rotation state:', state);
         if (state) {
             this.timer = this.timeout(this.rotate, this.rotations[this.current_screen].duration*1000);
-            
+
         } else {
             this.timeout.cancel(this.timer);
         }
-        
+
         this.enabled = state;
     }
 
