dbc404ce76b0223d9f17cd2bdfc1e27cfa971626
ri0t <riot@c-base.org>
2018-05-25 18:09:20 +0200

    Added multi editing and basic RBAC permission changing

--- src/objects/list/list.js
+++ src/objects/list/list.js
@@ -18,12 +18,13 @@
  */
 
 class objectlist {
-
-    constructor($stateParams, $rootScope, schemata, objectproxy, user) {
+    constructor($stateParams, $rootScope, schemata, objectproxy, user, notification, socket) {
         this.rootscope = $rootScope;
         this.schemata = schemata;
         this.objectproxy = objectproxy;
         this.user = user;
+        this.notification = notification;
+        this.socket = socket;
 
         this.schemaname = $stateParams.schema;
 
@@ -31,9 +32,13 @@ class objectlist {
 
         this.schemadata = {};
         this.filter = {};
-        this.fields = {};
+        this.fields = ['name', 'uuid', 'perms', 'owner'];
 
         this.objectlistdata = {};
+        this.objectlisttoggles = {};
+
+        this.select_state = false;
+        this.selected_permissions = {};
 
         if (user.signedin) {
             this.getData();
@@ -58,8 +63,14 @@ class objectlist {
             console.log('[OL] Objectmanager got a new list for ', schema);
             if (self.schemaname === schema) {
                 console.log('[OL] Received an object list');
-                self.objectlistdata = self.objectproxy.list(schema);
-                console.log('[OL] Data: ', self.objectlistdata);
+
+                self.objectlisttoggles = {};
+                self.objectlistdata = {};
+
+                for (let item of self.objectproxy.list(schema)) {
+                    self.objectlisttoggles[item.uuid] = false;
+                    self.objectlistdata[item.uuid] = item;
+                }
             }
         });
     }
@@ -74,6 +85,88 @@ class objectlist {
         this.objectproxy.deleteObject(this.schemaname, uuid);
     }
 
+    remove_role(uuid, action, role) {
+        console.log('[OL] Removing role:', uuid, action, role);
+
+        if (['admin', 'owner', 'system'].indexOf(role) >= 0) {
+            this.notification.add('warning', 'Cannot revoke', 'Admin and owner permissions cannot be revoked.', 5);
+            return
+        }
+
+        let self = this;
+
+        this.socket.send({
+            component: 'hfos.events.objectmanager',
+            action: 'remove_role',
+            data: {
+                schema: self.schemaname,
+                uuid: uuid,
+                action: action,
+                role: role
+            }
+        })
+    }
+
+    add_role(uuid, action, role) {
+        console.log('[OL] Adding role:', uuid, action, role);
+
+        if (['admin', 'owner', 'system'].indexOf(role) >= 0) {
+            this.notification.add('warning', 'Cannot add', 'Admin and owner permissions cannot be added.', 5);
+            return
+        }
+
+        let self = this;
+
+        this.socket.send({
+            component: 'hfos.events.objectmanager',
+            action: 'add_role',
+            data: {
+                schema: self.schemaname,
+                uuid: uuid,
+                action: action,
+                role: role
+            }
+        })
+    }
+
+    select_objects() {
+        this.selected_objects = [];
+
+        let self = this;
+
+        function update_selected_perms(obj) {
+            for (let action of ['write', 'read', 'list']) {
+                for (let role of obj.perms[action]) {
+                    if (self.selected_permissions[action].indexOf(role) < 0) {
+                        self.selected_permissions[action].push(role)
+                    }
+                }
+            }
+        }
+
+        this.selected_permissions = {
+            write: [],
+            read: [],
+            list: []
+        };
+
+        for (let uuid of Object.keys(this.objectlisttoggles)) {
+            if (this.objectlisttoggles[uuid] === true) this.selected_objects.push(uuid);
+            update_selected_perms(this.objectlistdata[uuid]);
+        }
+    }
+
+    select_all() {
+        console.log('[OL] Selecting all items');
+        this.select_state = !this.select_state;
+
+        for (let uuid of Object.keys(this.objectlisttoggles)) {
+            this.objectlisttoggles[uuid] = this.select_state;
+        }
+
+        this.select_objects();
+    }
+
     getData() {
         console.log('[OL] Requesting ObjectList.');
         this.schemadata = this.schemata.get(this.schemaname);
@@ -82,6 +175,6 @@ class objectlist {
 
 }
 
-objectlist.$inject = ['$stateParams', '$rootScope', 'schemata', 'objectproxy', 'user'];
+objectlist.$inject = ['$stateParams', '$rootScope', 'schemata', 'objectproxy', 'user', 'notification', 'socket'];
 
 export default objectlist;
